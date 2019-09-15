d7ce88ba56c870d844aaa6cca30879c6d8811624
Mathieu Keller <Mathieu Keller>
2019-09-15 21:04:16 +0200

    Add sass and burgerbuilder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const burgerBuilder = () => {
+    return (
+        <>
+            <div>Burger</div>
+            <div>Build Controls</div>
+        </>
+    );
+};
+
+export default burgerBuilder;
